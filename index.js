b0485bc6624457f88378fd0eb56d1a126d9d9b3a
Emerson Capuchi Romaneli <emerson.romaneli@gmail.com>
2024-03-05 00:39:16 -0300

    Fix app closing when all windows were closed

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const { Manager } = require('./modules/manager')
 
 const IS_DARWIN = process.platform === 'darwin'
 
-if (IS_DARWIN) {
-  app.dock.hide()
+app.whenReady().then(() => {
+  if (IS_DARWIN) {
+    app.dock.hide()
+  }
   app.on('window-all-closed', () => {})
-}
 
-app.whenReady().then(() => Manager.getInstance())
+  Manager.getInstance()
+})
