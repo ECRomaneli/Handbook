aa071be68dace870d004eb1f63e3f80c2b2546dd
Emerson Capuchi Romaneli <emerson.romaneli@gmail.com>
2024-06-18 19:28:12 -0300

    Created a new repository for the electron-findbar
    - Add accelerators for the common shortcuts.

--- index.js
+++ index.js
@@ -2,21 +2,11 @@ const { app } = require('electron')
 const { Manager } = require('./lib/manager')
 const { OS } = require('./lib/constants')
 
-const { Findbar } = require('./lib/findbar')
-
 app.whenReady().then(() => {
   if (OS.IS_DARWIN) {
     app.dock.hide()
   }
   app.on('window-all-closed', () => {})
-  testFindBar()
 
   Manager.getInstance()
 })
-
-function testFindBar() {
-  const { BrowserWindow } = require('electron')
-  const window = new BrowserWindow()
-  window.loadURL('https://github.com')
-  Findbar.open(window)
-}
