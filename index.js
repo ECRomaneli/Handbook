09bbc73c06e7e7761f8983db8448cd03e9ca972b
Emerson Capuchi Romaneli <emerson.romaneli@gmail.com>
2025-05-06 06:07:56 -0300

    Fix startup

--- index.js
+++ index.js
@@ -3,20 +3,24 @@ import { app, globalShortcut } from 'electron'
 import { OS } from './lib/constants.js';
 import { Manager } from './lib/manager.js';
 
-if (!app.requestSingleInstanceLock()) {
-  console.error('Another instance is already running')
-  app.quit()
-}
-
-app.on('window-all-closed', () => {})
-app.on('quit', () => { globalShortcut.unregisterAll() })
-
 app.whenReady().then(() => {
+  if (!app.requestSingleInstanceLock()) {
+    console.error('Another instance is already running')
+    app.quit()
+    return
+  }
+
   if (!OS.IS_WIN32) {
     if (OS.IS_DARWIN) { app.dock.hide() }
-    Manager.getInstance()
+    startManager()
   } else {
     // Squirrel startup handling
-    import('electron-squirrel-startup').then(m => { m.default ? app.quit() : Manager.getInstance() })
+    import('electron-squirrel-startup').then(m => { m.default ? app.quit() : startManager() })
   }
 })
+
+function startManager() {
+  app.on('window-all-closed', () => {})
+  app.on('quit', () => { globalShortcut.unregisterAll() })
+  Manager.getInstance()
+}
