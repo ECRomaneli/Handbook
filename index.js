5d4e0695ff85cbbca379f059b0cd96b078d55714
Emerson Capuchi Romaneli <emerson.romaneli@gmail.com>
2025-11-05 07:21:52 -0300

    snapshot last changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { app, globalShortcut } from 'electron'
+import { app, BaseWindow, BrowserWindow, globalShortcut, WebContentsView } from 'electron'
 
 import { OS } from './lib/constants.js'
 import Manager from './lib/manager.js'
@@ -25,4 +25,22 @@ function startManager() {
   app.on('window-all-closed', () => {})
   app.on('quit', () => { globalShortcut.unregisterAll() })
   Manager.start()
+}
+
+function test() {
+  const window = new BaseWindow({ width: 800, height: 600 })
+  const browser = new BrowserWindow({ width: 800, height: 600 })
+
+  const view = new WebContentsView()
+  view.webContents.loadURL('https://github.com')
+  window.contentView.addChildView(view)
+  console.log('browser', BrowserWindow.getAllWindows().length)
+  console.log('base', BaseWindow.getAllWindows().length)
+  console.log(window.webContents)
+
+  console.log('from webcontents', getBaseWindowFromWebContents(view.webContents) === window)
+}
+
+function getBaseWindowFromWebContents(webContents) {
+  return BaseWindow.getAllWindows().find(win => win.webContents === webContents || win.contentView.children.some(child => child.webContents === webContents))
 }
